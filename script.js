40a1ff1bd02e5067c776d3fc55284070472bfa6e
Dmitri Qiu <dmitrimeowsalot@gmail.com>
2019-05-25 20:09:28 +1000

    Fixed loading bug

--- script.js
+++ script.js
@@ -2,23 +2,19 @@ window.onload = function() {
     console.log("Loaded");
     let loader = document.getElementById("loader");
     loader.style.left = "200%";
-    setTimeout(()=>{
-        loader.style.display = "none";
-    }, 1000);
-    document.getElementById("content").style.display = "block";
     let about_heading = document.getElementById("about");
     let skills_heading = document.getElementById("skills");
     let links_heading = document.getElementById("linksheader");
     makeLonger(about_heading);
     makeLonger(skills_heading);
     makeLonger(links_heading);
-}
+};
 
 function makeLonger(elem) {
     let width = elem.offsetWidth;
     let original = elem.innerHTML;
     let string = "";
-    for (let i = 0; i < window.width / width + 1; i++) {
+    for (let i = 0; i < window.innerWidth / width + 1; i++) {
         if (i == 1) {
             string += `<span class=\"highlight\" style=\"border-bottom: 5px solid #b43fbd\">${original}</span>`;
         } else {
